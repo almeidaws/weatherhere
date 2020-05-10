0e707619e05dd8e7c82ea1e6b6e648ac2c1fa8e0
Gustavo Almeida <almeidaws@outlook.com>
2020-05-10 14:50:58 -0300

    Create UserInterface module with inside workspace with only initial config

---
+++ UserInterface/UserInterface/LocalWeatherViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocalWeatherViewController.swift
+//  UserInterface
+//
+//  Created by Gustavo Amaral on 10/05/20.
+//  Copyright © 2020 Gustavo Almeida Amaral. All rights reserved.
+//
+
+import UIKit
+
+class LocalWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
