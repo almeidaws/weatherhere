1bfabe052a6494c04d58f42c7953494acf8bf3fe
Gustavo Almeida <almeidaws@outlook.com>
2020-05-10 20:39:31 -0300

    Create Services module

---
+++ Services/Services/Services.h
@@ -0,0 +1,19 @@
+//
+//  Services.h
+//  Services
+//
+//  Created by Gustavo Amaral on 10/05/20.
+//  Copyright © 2020 Gustavo Almeida Amaral. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Services.
+FOUNDATION_EXPORT double ServicesVersionNumber;
+
+//! Project version string for Services.
+FOUNDATION_EXPORT const unsigned char ServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Services/PublicHeader.h>
+
+
---
+++ Services/Services/Services.swift
@@ -0,0 +1,43 @@
+//
+//  Services.swift
+//  Services
+//
+//  Created by Gustavo Amaral on 10/05/20.
+//  Copyright © 2020 Gustavo Almeida Amaral. All rights reserved.
+//
+
+import Foundation
+
+public protocol Service { }
+
+public class Services {
+    
+    public static let `default` = Services()
+    private var registers = [ObjectIdentifier : () -> Service]()
+    
+    public func register<S, I>(_ service: S.Type, maker: @escaping () -> I) where S: Service, I: Service {
+        registers[ObjectIdentifier(service)] = maker
+    }
+    
+    public func make<S, I>(for service: S.Type) -> I where S: Service, I: Service {
+        let id = ObjectIdentifier(service)
+        guard let maker = registers[id] else {
+            fatalError("Service '\(service)' wasn't previously registered")
+        }
+        guard let casted = maker() as? I else {
+            fatalError("Service '\(service)' can't be downcasted to '\(I.self)'.")
+        }
+        return casted
+    }
+}
+
+public extension Services {
+    static func register<S, I>(_ service: S.Type, maker: @escaping () -> I) where S: Service, I: Service {
+        Services.default.register(service, maker: maker)
+    }
+    
+    static func make<S, I>(for service: S.Type) -> I where S: Service, I: Service {
+        return Services.default.make(for: service)
+    }
+}
+
