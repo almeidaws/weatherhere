5a4ab6ef4c806ce36223bb3db3f854b9eaa48228
Gustavo Almeida <almeidaws@outlook.com>
2020-05-14 10:41:03 -0300

    Remove dependecy between Storage, Networking and GPS module with Services module

---
+++ UserInterface/UserInterface/CoreLocationGPS+Service.swift
@@ -0,0 +1,9 @@
+//
+//  CoreLocationGPS+Service.swift
+//  UserInterface
+//
+//  Created by Gustavo Amaral on 14/05/20.
+//  Copyright © 2020 Gustavo Almeida Amaral. All rights reserved.
+//
+
+import Foundation
---
+++ UserInterface/UserInterface/SQLiteStorage+Service.swift
@@ -0,0 +1,9 @@
+//
+//  SQLiteStorage+Service.swift
+//  UserInterface
+//
+//  Created by Gustavo Amaral on 14/05/20.
+//  Copyright © 2020 Gustavo Almeida Amaral. All rights reserved.
+//
+
+import Foundation
